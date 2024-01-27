ccd665b4fda0dfd137aecf11f0f5574b8695a653
HarshitSinghai <harshitksinghai.official@gmail.com>
2024-01-27 16:54:24 +0530

    1-1. add comments to server start code

--- server/server.js
+++ server/server.js
@@ -1,15 +1,15 @@
 import express from "express";
-import dotenv from 'dotenv';
+import dotenv from 'dotenv'; // loads environment variables from a .env file into process.env. 
 
-dotenv.config();
+dotenv.config(); // reads the .env file, assigns the key-value pairs in .env file to the process.env object
 
-const app = express();
+const app = express(); // express() function returns an object that is referenced by variable 'app' (app can be called an object as it is referencing an object). The object contains methods and properties.
 const port = process.env.PORT;
 
-app.get('/', (req, res) => {
+app.get('/', (req, res) => { // when client/user access URL '/' for HTTP GET request, this function will execute // req = request object representing HTTP request from client // res = response object representing the HTTP response that will be sent to the client.
     res.send("Server up and running!");
 });
 
-app.listen(port, ()=>{
+app.listen(port, ()=>{ // This line starts the Express application to listen for incoming HTTP requests on the specified port (port).
     console.log("Server running on port 3000");
 });
