e13a343a24639c27955969c3065a06c161cefff3
HarshitSinghai <harshitksinghai.official@gmail.com>
2024-01-27 17:21:48 +0530

    1-2. add body parsing middlewares - JSON, URL-encoded form data

--- server/server.js
+++ server/server.js
@@ -6,6 +6,9 @@ dotenv.config(); // reads the .env file, assigns the key-value pairs in .env fil
 const app = express(); // express() function returns an object that is referenced by variable 'app' (app can be called an object as it is referencing an object). The object contains methods and properties.
 const port = process.env.PORT;
 
+app.use(express.json()); // The express.json() middleware checks the Content-Type header of the incoming request. If the Content-Type is application/json, it attempts to parse the request body as JSON and populates req.body with the parsed JSON data. If the Content-Type is not application/json, the middleware ignores the request body, and req.body remains an empty object.
+app.use(express.urlencoded({extended: true})); // The express.urlencoded({ extended: true }) middleware checks the Content-Type header of the incoming request. If the Content-Type is application/x-www-form-urlencoded, it attempts to parse the request body as URL-encoded data and populates req.body with the parsed data. If the Content-Type is not application/x-www-form-urlencoded, the middleware ignores the request body, and req.body remains an empty object.
+
 app.get('/', (req, res) => { // when client/user access URL '/' for HTTP GET request, this function will execute // req = request object representing HTTP request from client // res = response object representing the HTTP response that will be sent to the client.
     res.send("Server up and running!");
 });
