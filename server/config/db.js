5578dee269ca2f46f17290e5ed91aab34ebabb99
HarshitSinghai <harshitksinghai.official@gmail.com>
2024-01-28 12:34:49 +0530

    3-2. install mongoose module, add MONGO_URI in .env file, write boilerplate code to connect mongoDB atlas (connectDB function) in db.js, import connectDB in server.js, add connectDB() function in server.js to establish connection to the database

---
+++ server/config/db.js
@@ -0,0 +1,14 @@
+// create mongodb atlas database first
+import mongoose, {mongo} from 'mongoose';
+
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URI);
+    console.log(`Connected to MongoDB: ${conn.connection.host}`);
+  } catch (error) {
+    console.error(`Error connecting to MongoDB: ${error.message}`);
+    process.exit(1);
+  }
+};
+
+export default connectDB;
--- server/server.js
+++ server/server.js
@@ -1,9 +1,10 @@
 import express from "express";
 import dotenv from 'dotenv'; // loads environment variables from a .env file into process.env. 
 import userRoutes from '.routes/userRoutes.js';
+import connectDB from './config/db.js';
 
 dotenv.config(); // reads the .env file, assigns the key-value pairs in .env file to the process.env object
-
+connectDB();
 const app = express(); // express() function returns an object that is referenced by variable 'app' (app can be called an object as it is referencing an object). The object contains methods and properties.
 const port = process.env.PORT;
 
